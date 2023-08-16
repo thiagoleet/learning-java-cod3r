ca4204b3cc183403871b2a92ebf87164730a6359
Thiago Leite <thiagofmleite@gmail.com>
2023-08-16 18:03:12 -0300

    Streams: Reduce com pipeline

---
+++ exercicios/src/streams/UsandoReduce2.java
@@ -0,0 +1,28 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class UsandoReduce2 {
+    public static void main(String[] args) {
+        Aluno a1 = new Aluno("Ana", 7.1);
+        Aluno a2 = new Aluno("Luana", 6.1);
+        Aluno a3 = new Aluno("Gui", 8.1);
+        Aluno a4 = new Aluno("Gabi", 10);
+
+        List<Aluno> alunos = Arrays.asList(a1, a2, a3, a4);
+
+        Predicate<Aluno> isAprovado = a -> a.nota >= 7;
+        Function<Aluno, Double> getNota = a -> a.nota;
+        BinaryOperator<Double> somatorio = (a, b) -> a + b;
+
+        alunos.parallelStream()
+                .filter(isAprovado)
+                .map(getNota)
+                .reduce(somatorio)
+                .ifPresent(System.out::println);
+    }
+}
