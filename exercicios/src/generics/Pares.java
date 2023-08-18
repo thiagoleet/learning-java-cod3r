1efce55fec3a0dd6d306379f01bb74a15960f65c
Thiago Leite <thiagofmleite@gmail.com>
2023-08-18 19:08:01 -0300

    fixed imports

--- exercicios/src/generics/Pares.java
+++ exercicios/src/generics/Pares.java
@@ -1,8 +1,12 @@
 package generics;
-import java.util.*;
+
+
+import java.util.LinkedHashSet;
+import java.util.Optional;
+import java.util.Set;
 
 public class Pares<C extends Number, V> {
-    private final Set<Par<C, V>> itens = new HashSet<>();
+    private final Set<Par<C, V>> itens = new LinkedHashSet<>();
 
     public void adicionar(C chave, V valor) {
         if (chave == null) return; // Saída do método
