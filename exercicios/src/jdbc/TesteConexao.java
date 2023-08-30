79f14ec5aa42f7cf9185b9b94109dc103c5fc28f
Thiago Leite <thiagofmleite@gmail.com>
2023-08-30 17:33:56 -0300

    Criando uma Fábrica de Conexão

--- exercicios/src/jdbc/TesteConexao.java
+++ exercicios/src/jdbc/TesteConexao.java
@@ -6,12 +6,16 @@ import java.sql.SQLException;
 
 public class TesteConexao {
     public static void main(String[] args) throws SQLException {
+        /*
         final String url = "jdbc:mysql://localhost:3306?verifyServerCertificate=false&useSSL=true";
         final String usuario = "root";
         final String senha = "root";
 
         // gera uma exeção
         Connection conexao = DriverManager.getConnection(url, usuario, senha);
+         */
+
+        Connection conexao = FabricaConexao.getConexao();
 
         System.out.println("Conexão efetuada com sucesso!");
         conexao.close();
