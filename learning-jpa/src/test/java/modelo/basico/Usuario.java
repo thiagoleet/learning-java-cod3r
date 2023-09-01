e9e9147c75a69727d14b6c2efbd35028fe0000e1
Thiago Leite <thiagofmleite@gmail.com>
2023-09-01 11:00:39 -0300

    Adicionando dependências

--- learning-jpa/src/test/java/modelo/basico/Usuario.java
+++ learning-jpa/src/test/java/modelo/basico/Usuario.java
@@ -1,15 +1,17 @@
 package modelo.basico;
 
 
-import javax.persistence;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Entity
 public class Usuario {
+    @Id
     private Long id;
     private String nome;
     private String email;
 
     public Usuario() {
-
     }
 
 
