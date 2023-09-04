7b3a3cdd12bc33980e826d580475009a1a033fda
Thiago Leite <thiagofmleite@gmail.com>
2023-09-04 16:10:26 -0300

    Native Named Query

--- learning-jpa/src/infra/DAO.java
+++ learning-jpa/src/infra/DAO.java
@@ -86,4 +86,10 @@ public class DAO<E> {
         return query.getResultList();
     }
 
+    public E consultarUm(String nomeConsulta, Object... params) {
+        List<E> lista = consultar(nomeConsulta, params);
+
+        return lista.isEmpty() ? null : lista.get(0);
+    }
+
 }
