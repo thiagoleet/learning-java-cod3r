4717a5dd0de2025e9c0c8920511e3b812ab75391
Thiago Leite <thiagofmleite@gmail.com>
2023-09-05 10:04:11 -0300

    Exports to

--- modularidade/app-calculo/src/module-info.java
+++ modularidade/app-calculo/src/module-info.java
@@ -2,4 +2,7 @@ module app.calculo {
     requires app.loggin;
     exports me.thiagoleite.app.calculo;
 
+    exports me.thiagoleite.app.calculo.interno
+            to app.financeiro;
+
 }
--- modularidade/app-financeiro/src/me/thiagoleite/app/financeiro/Teste.java
+++ modularidade/app-financeiro/src/me/thiagoleite/app/financeiro/Teste.java
@@ -1,10 +1,15 @@
 package me.thiagoleite.app.financeiro;
 
 import me.thiagoleite.app.calculo.Calculadora;
+import me.thiagoleite.app.calculo.interno.OperacoesAritimeticas;
 
 public class Teste {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
         System.out.println(calculadora.soma(2, 3, 4));
+
+        // usando exports to
+        OperacoesAritimeticas op = new OperacoesAritimeticas();
+        System.out.println(op.soma(2,5));
     }
 }
