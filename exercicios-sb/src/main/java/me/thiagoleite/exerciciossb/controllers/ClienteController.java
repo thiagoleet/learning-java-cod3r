50d73682e4c4c1d772775be4d77a143320dd55e1
Thiago Leite <thiagofmleite@gmail.com>
2023-09-25 20:45:41 -0300

    Usando parâmetros no método GET

--- exercicios-sb/src/main/java/me/thiagoleite/exerciciossb/controllers/ClienteController.java
+++ exercicios-sb/src/main/java/me/thiagoleite/exerciciossb/controllers/ClienteController.java
@@ -1,10 +1,7 @@
 package me.thiagoleite.exerciciossb.controllers;
 
 import me.thiagoleite.exerciciossb.models.Cliente;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/clientes")
@@ -17,6 +14,13 @@ public class ClienteController {
 
     @GetMapping("/{id}")
     public Cliente obterClientePorId(@PathVariable int id) {
+        return new Cliente(id, "Pedro", "123.456.789-00");
+    }
+
+    @GetMapping
+    public Cliente obterCliente(
+            @RequestParam(name = "id") int id
+    ) {
         return new Cliente(id, "Pedro", "123.456.789-00");
     }
 }
