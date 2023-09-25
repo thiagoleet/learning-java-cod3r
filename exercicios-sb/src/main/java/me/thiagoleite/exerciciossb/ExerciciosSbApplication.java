02965b1b752072bc1cbf3232cbabcdd89d809d09
Thiago Leite <thiagofmleite@gmail.com>
2023-09-25 19:49:17 -0300

    Iniciando com Spring Boot

---
+++ exercicios-sb/src/main/java/me/thiagoleite/exerciciossb/ExerciciosSbApplication.java
@@ -0,0 +1,15 @@
+package me.thiagoleite.exerciciossb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class ExerciciosSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciciosSbApplication.class, args);
+	}
+
+}
---
+++ exercicios-sb/src/main/java/me/thiagoleite/exerciciossb/controllers/PrimeiroController.java
@@ -0,0 +1,13 @@
+package me.thiagoleite.exerciciossb.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrimeiroController {
+    @GetMapping(path = "/ola")
+    public String ola() {
+        return "Olá Spring Boot";
+    }
+}
