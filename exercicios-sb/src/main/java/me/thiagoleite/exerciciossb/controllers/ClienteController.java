ca4bf699bc9a61859f1e0491443221c6aa8b8ac3
Thiago Leite <thiagofmleite@gmail.com>
2023-09-25 20:37:08 -0300

    Usando parâmetros de rota

--- exercicios-sb/src/main/java/me/thiagoleite/exerciciossb/controllers/ClienteController.java
+++ exercicios-sb/src/main/java/me/thiagoleite/exerciciossb/controllers/ClienteController.java
@@ -2,6 +2,7 @@ package me.thiagoleite.exerciciossb.controllers;
 
 import me.thiagoleite.exerciciossb.models.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/clientes")
 public class ClienteController {
 
-    @GetMapping(path = "/qualquer")
+    @GetMapping("/qualquer")
     public Cliente obterCliente() {
         return new Cliente(28, "Pedro", "123.456.789-00");
+    }
+
+    @GetMapping("/{id}")
+    public Cliente obterClientePorId(@PathVariable int id) {
+        return new Cliente(id, "Pedro", "123.456.789-00");
     }
 }
