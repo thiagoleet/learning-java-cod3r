c5daab40df65338e426f886305c9b91145709f99
Thiago Leite <thiagofmleite@gmail.com>
2023-09-25 19:54:28 -0300

    Usando outros métodos HTTP

--- exercicios-sb/src/main/java/me/thiagoleite/exerciciossb/controllers/PrimeiroController.java
+++ exercicios-sb/src/main/java/me/thiagoleite/exerciciossb/controllers/PrimeiroController.java
@@ -2,12 +2,18 @@ package me.thiagoleite.exerciciossb.controllers;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PrimeiroController {
     @GetMapping(path = "/ola")
     public String ola() {
+        return "Olá Spring Boot";
+    }
+
+    @PostMapping(path = "/ola")
+    public String saudacao() {
         return "Olá Spring Boot";
     }
 }
