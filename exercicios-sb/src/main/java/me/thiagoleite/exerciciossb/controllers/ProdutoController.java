8b350851c5750b051d82bad14c1b47813bb93df9
Thiago Leite <thiagofmleite@gmail.com>
2023-09-26 10:48:36 -0300

    Buscando um produto por ID

--- exercicios-sb/src/main/java/me/thiagoleite/exerciciossb/controllers/ProdutoController.java
+++ exercicios-sb/src/main/java/me/thiagoleite/exerciciossb/controllers/ProdutoController.java
@@ -6,6 +6,8 @@ import me.thiagoleite.exerciciossb.model.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutoController {
@@ -22,5 +24,10 @@ public class ProdutoController {
     @GetMapping
     public Iterable<Produto> obterProdutos() {
         return produtoRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
+        return produtoRepository.findById(id);
     }
 }
