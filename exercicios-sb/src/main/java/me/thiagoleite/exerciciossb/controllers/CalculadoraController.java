41bf29c5ae77d76eecd2e1d3a3aea93d757fe2f5
Thiago Leite <thiagofmleite@gmail.com>
2023-09-26 07:27:34 -0300

    Desafio: Calculadora

---
+++ exercicios-sb/src/main/java/me/thiagoleite/exerciciossb/controllers/CalculadoraController.java
@@ -0,0 +1,17 @@
+package me.thiagoleite.exerciciossb.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/calculadora")
+public class CalculadoraController {
+    @GetMapping("/somar/{a}/{b}")
+    public int somar(@PathVariable int a, @PathVariable int b) {
+        return a + b;
+    }
+
+    @GetMapping("/subtrair")
+    public int subtrair(@RequestParam(name = "a") int a, @RequestParam(name = "b") int b) {
+        return a - b;
+    }
+}
